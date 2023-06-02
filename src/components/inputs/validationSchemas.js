409c553e3177cc59a744a9d2530f644803a181d4
xSjnxyz <mailme.suzan77@gmail.cmo>
2023-06-02 19:29:15 +0545

    chore: add basic validation schemas

---
+++ src/components/inputs/validationSchemas.js
@@ -0,0 +1,15 @@
+export const passwordSchema = {
+  pattern: {
+    value: /^[a-zA-Z0-9!@#$%^&*()_+{}[\]:;'"\\|,<.>/?]{8,}$/,
+    message: "password is weak",
+  },
+  required: "password is required",
+};
+
+export const emailSchema = {
+  pattern: {
+    value: /^[\w-.]+@([\w-]+\.)+[\w-]{2,}$/i,
+    message: "email must be valid.",
+  },
+  required: "email is required",
+};
