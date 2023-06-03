735f9bfea431aaa9afcc492df41ca699b0d17edb
xSjnxyz <mailme.suzan77@gmail.cmo>
2023-06-03 12:50:52 +0545

    chore: component route

--- src/config/url.js
+++ src/config/url.js
@@ -1,14 +1,30 @@
-const ROOT_URL = '/';
+const ROOT_URL = "/";
 
 // Employee
-const EMPLOYEE_FORM = '/employee/form';
-const EMPLOYEE_LIST_URL = '/employees';
+const EMPLOYEE_FORM = "/employee/form";
+const EMPLOYEE_LIST_URL = "/employees";
 
 // User
-const FEED_URL = '/feed';
-const PROFILE_URL = '/profile';
+const FEED_URL = "/feed";
+const PROFILE_URL = "/profile";
 
 // Auth
-const LOGIN_URL = '/login';
+const LOGIN_URL = "/login";
 
-export {ROOT_URL, EMPLOYEE_FORM, EMPLOYEE_LIST_URL, LOGIN_URL, FEED_URL, PROFILE_URL};
+// component namespace
+const COMPONENT_URL = {
+  ROOT: "/components",
+  RADIO: "radio",
+  CHECKBOX: "checkbox",
+  INPUT: "input",
+};
+
+export {
+  COMPONENT_URL,
+  EMPLOYEE_FORM,
+  EMPLOYEE_LIST_URL,
+  FEED_URL,
+  LOGIN_URL,
+  PROFILE_URL,
+  ROOT_URL,
+};
